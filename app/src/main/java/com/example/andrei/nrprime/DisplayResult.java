a5dfaa380606f3400a899dde08d94f41e492b4bd
AndreiF05 <AndreiF05@users.noreply.github.com>
2018-11-23 22:14:28 +0200

    Nr prime

--- app/src/main/java/com/example/andrei/nrprime/DisplayResult.java
+++ app/src/main/java/com/example/andrei/nrprime/DisplayResult.java
@@ -1,39 +1,53 @@
 package com.example.andrei.nrprime;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class DisplayResult extends AppCompatActivity {
-
+    public String message;
+    public TextView result;
+    public ConstraintLayout current;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_result);
-
-
+        result = findViewById(R.id.result);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
+        message = intent.getStringExtra("mesaj");
         TextView number = findViewById(R.id.textView);
         number.setText(message);
+        current = findViewById(R.id.the_layout);
+        prime();
     }
-    TextView result = (TextView) findViewById(R.id.result);
-    public void prime(View view){
-        int nr = Integer.parseInt(MainActivity.EXTRA_MESSAGE);
+
+    public void prime(){
+        double nr = Double.parseDouble(message);
+        int nri = (int)nr;
         boolean prime = true;
-        for (int i = 2; nr <= i; i++) {
+        for (int i = 2; i * i <= nr; i++) {
             if (nr % i == 0 ) {
                 prime = false;
                 break;
             }
+        }
+
+        if(nr!=nri)prime=false;
+        if(nr<0)prime=false;
+
+        if(prime){
+                this.result.setText("nr prim");
+                current.setBackgroundColor(Color.parseColor("#59ff74"));
+        }
+        else{
+                this.result.setText("nu e nr prim");
+                current.setBackgroundColor(Color.parseColor("#e01600"));
+        }
     }
-    if(prime){
-            result.setText("nr prim");
-    }
-    else{
-            result.setText("nu e nr prim");
-    }
-}}
+}
--- app/src/main/java/com/example/andrei/nrprime/MainActivity.java
+++ app/src/main/java/com/example/andrei/nrprime/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.andrei.nrprime;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.andrei.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-        public void sendMessage(View view) {
-            Intent intent = new Intent(this, DisplayResult.class);
-            EditText editText = (EditText) findViewById(R.id.editText);
-            String message = editText.getText().toString();
-            intent.putExtra(EXTRA_MESSAGE, message);
+    public void sendMessage(View view) {
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = String.valueOf(editText.getText());
+        if(message.matches("-?\\d+(\\.\\d+)?")) {
+            Intent intent = new Intent(MainActivity.this, DisplayResult.class);
+            intent.putExtra("mesaj", message);
             startActivity(intent);
+        }
+        else showToast();
+    }
+    public void showToast(){
+        Context context = getApplicationContext();
+        CharSequence text = "Nu este numar!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
